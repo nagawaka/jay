09d151a9118afabe6792806afdcfe1aad81ef9b8
Nagawaka <nakagawa@gmail.com>
2019-09-23 19:03:03 -0300

    update gitignore, lastm integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `nagawaka`,
@@ -20,6 +24,14 @@ module.exports = {
         resources: 'src/styles/resources/main.scss'
       }
     },
+    {
+      resolve: "gatsby-source-lastfm",
+      options: {
+        api_key: `${process.env.LAST_FM_API_KEY}`,
+        username: `${process.env.LAST_FM_USERNAME}`,
+        limit: 10, // the maximum number of playbacks to pull in
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -31,7 +43,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,50 @@
+import { useStaticQuery, graphql } from "gatsby"
+
 import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allLastfmPlayback {
+      edges {
+        node {
+          id
+          track {
+            id
+            name
+            url
+            image {
+              text
+              size
+            }
+            artist {
+              id
+              name
+            }
+            album {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+  `)
 
-    <h1>Hello world!</h1>
-    <p>Welcome to my little playpen.</p>
-  </Layout>
-)
+  console.log(data);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <h1>Hello world!</h1>
+      <p>Welcome to my little playpen.</p>
+    </Layout>
+  )
+}
 
 export default IndexPage
